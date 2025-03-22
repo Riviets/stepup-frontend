ac4be87d43c21a620da895f334668687eb0cc885
Riviets <vladvladpobuta@gmal.com>
2025-03-22 19:31:35 +0200

    Edit UserStats component to update it every time a habit in the tracker is completed

--- src/components/layout/UserStats.jsx
+++ src/components/layout/UserStats.jsx
@@ -2,20 +2,24 @@ import { useState, useEffect } from "react";
 import useFetch from "../hooks/useFetch";
 import Spinner from "./Spinner";
 import { authService } from "../../services/authService";
-import xp from '../../assets/xp.png'
-import coins from '../../assets/coins.png'
+import xp from '../../assets/xp.png';
+import coins from '../../assets/coins.png';
 
-export default function UserStats(){
-    const {data: userData, isLoading, error} = useFetch(authService.getCurrentUser)
+export default function UserStats({ refetchUserData }) { 
+    const { data: userData, isLoading, error, refetch } = useFetch(authService.getCurrentUser);
 
-    if(isLoading) return (<div className="flex justify-center"><Spinner /></div>)
-    if(error) return (<div>{error.message}</div>)
-    
-    return(
+    if (refetchUserData) {
+        refetchUserData.current = refetch; 
+    }
+
+    if (isLoading) return <div className="flex justify-center"><Spinner /></div>;
+    if (error) return <div>{error.message}</div>;
+
+    return (
         <div className="flex justify-between max-w-[350px] border-2 border-[#483D61] items-center px-[25px] mx-auto rounded-lg bg-[#D9D9D9] font-bold text-lg mb-[35px]">
             <div className="flex items-center gap-2">
                 <p>{userData?.xp}</p>
-                <img className="min-w-[25px]" src={xp} alt="xp"/>
+                <img className="min-w-[25px]" src={xp} alt="xp" />
             </div>
             <div className="flex items-center">
                 <p>{userData?.currency}</p>
@@ -23,5 +27,5 @@ export default function UserStats(){
             </div>
             <p className="text-xl">{userData?.username}</p>
         </div>
-    )
+    );
 }
