8c42aa1b19c32bf256cba7ef370f9709566f7b2b
Riviets <vladvladpobuta@gmal.com>
2025-05-14 21:23:01 +0300

    Prepping for hosting

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { API_URL } from "../constants";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "https://stepup-api.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
