53cae951c2046270d8f3732cf2d202287ee52644
Riviets <vladvladpobuta@gmail.com>
2025-05-16 17:52:43 +0300

    Changed the api url to display profile pictures for hosted website

--- src/components/friends/SuggestHabitModal.jsx
+++ src/components/friends/SuggestHabitModal.jsx
@@ -1,54 +1,66 @@
-import { useState } from "react"
-import { useTranslation } from 'react-i18next';
-import close from "../../assets/close.svg"
-import { habitsService } from "../../services/habitsService"
-import useFetch from "../hooks/useFetch"
-import { Dropdown, DropdownItem } from "../utils/Dropdown"
-import MessageModal from "../layout/MessageModal"
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import close from "../../assets/close.svg";
+import { habitsService } from "../../services/habitsService";
+import useFetch from "../hooks/useFetch";
+import { Dropdown, DropdownItem } from "../utils/Dropdown";
+import MessageModal from "../layout/MessageModal";
 
 export default function SuggestHabitModal({ onClose, friend }) {
   const { t } = useTranslation();
-  const { data: customHabits, isLoading } = useFetch(habitsService.getUserHabits)
-  const [selectedHabitId, setSelectedHabitId] = useState(null)
-  const [isSent, setIsSent] = useState(false)
-  const [isMessageModalVisible, setIsMessageModalVisible] = useState(false)
-  const [message, setMessage] = useState("")
+  const { data: customHabits, isLoading } = useFetch(
+    habitsService.getUserHabits
+  );
+  const [selectedHabitId, setSelectedHabitId] = useState(null);
+  const [isSent, setIsSent] = useState(false);
+  const [isMessageModalVisible, setIsMessageModalVisible] = useState(false);
+  const [message, setMessage] = useState("");
 
   const handleSend = async () => {
     if (!selectedHabitId) {
-      setMessage(t('suggestHabitModal.errors.habitRequired'))
-      setIsMessageModalVisible(true)
-      return
+      setMessage(t("suggestHabitModal.errors.habitRequired"));
+      setIsMessageModalVisible(true);
+      return;
     }
 
-    setIsSent(true)
+    setIsSent(true);
     try {
-      console.log(`Friend Id: ${friend?.id}, Habit id: ${selectedHabitId}`)
-      const response = await habitsService.suggestHabit(friend?.id, selectedHabitId)
-      setMessage(t('suggestHabitModal.success'))
-      setSelectedHabitId(null)
+      console.log(`Friend Id: ${friend?.id}, Habit id: ${selectedHabitId}`);
+      const response = await habitsService.suggestHabit(
+        friend?.id,
+        selectedHabitId
+      );
+      setMessage(t("suggestHabitModal.success"));
+      setSelectedHabitId(null);
     } catch (error) {
-      setMessage(t('suggestHabitModal.errors.alreadySuggested'))
-      console.log(error)
+      setMessage(t("suggestHabitModal.errors.alreadySuggested"));
+      console.log(error);
     } finally {
-      setIsSent(false)
-      setIsMessageModalVisible(true)
+      setIsSent(false);
+      setIsMessageModalVisible(true);
     }
-  }
+  };
 
   const handleSelect = (habitId) => {
-    setSelectedHabitId(habitId)
-  }
+    setSelectedHabitId(habitId);
+  };
 
   return (
-    <div className="flex items-center justify-center fixed inset-0 z-10" style={{ backgroundColor: "rgba(0,0,0,0.6)" }}>
+    <div
+      className="flex items-center justify-center fixed inset-0 z-10"
+      style={{ backgroundColor: "rgba(0,0,0,0.6)" }}
+    >
       <div className="bg-[#D9D9D9] rounded-md border-2 border-[#292139] w-full max-w-[370px] h-[300px] relative pb-10 pt-15 px-5 relative">
         <p className="font-bold mb-5 text-2xl max-w-[300px] mx-auto text-center">
-          {t('suggestHabitModal.title', { username: friend?.username })}
+          {t("suggestHabitModal.title", { username: friend?.username })}
         </p>
         <div className="absolute z-10 w-full max-w-[325px]">
           <Dropdown
-            trigger={<button className="w-full py-3 bg-white text-xl rounded-md font-bold">{t('suggestHabitModal.select')}</button>}
+            trigger={
+              <button className="w-full py-3 bg-white text-xl rounded-md font-bold">
+                {t("suggestHabitModal.select")}
+              </button>
+            }
             handleSelect={handleSelect}
           >
             {customHabits?.map((habit) => (
@@ -63,14 +75,23 @@ export default function SuggestHabitModal({ onClose, friend }) {
             className="w-full text-center bg-purple-700 border-2 border-purple-900 text-white font-bold tracking-widest text-xl py-2 rounded-md absolute bottom-5 max-w-[200px]"
             disabled={isSent}
           >
-            {t('suggestHabitModal.send')}
+            {t("suggestHabitModal.send")}
           </button>
         </div>
         <button onClick={onClose} className="absolute top-5 right-7">
-          <img src={close} alt={t('suggestHabitModal.closeAlt')} className="min-w-[15px]" />
+          <img
+            src={close}
+            alt={t("suggestHabitModal.closeAlt")}
+            className="min-w-[15px]"
+          />
         </button>
       </div>
-      {isMessageModalVisible && <MessageModal message={message} onClose={() => setIsMessageModalVisible(false)} />}
+      {isMessageModalVisible && (
+        <MessageModal
+          message={message}
+          onClose={() => setIsMessageModalVisible(false)}
+        />
+      )}
     </div>
-  )
+  );
 }
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,56 +1,57 @@
-export const CARDS_NUMBER = 10
-export const PUZZLES_NUMBER = 16
-export const PUZZLE_SETS_NUMBER = 4
-export const PUZZLES_IN_SET = 4
-export const MAX_LEVEL = 20
-export const REACT_APP_API_URL='http://localhost:3000'
+export const CARDS_NUMBER = 10;
+export const PUZZLES_NUMBER = 16;
+export const PUZZLE_SETS_NUMBER = 4;
+export const PUZZLES_IN_SET = 4;
+export const MAX_LEVEL = 20;
+export const REACT_APP_API_URL = "https://stepup-api.onrender.com/";
 
 export const ACHIEVEMENTS = (t) => [
-    {
-        title: t('achievements.customer.title'),
-        description: t('achievements.customer.description'),
-        condition: (userCards) => userCards.length >= 5
+  {
+    title: t("achievements.customer.title"),
+    description: t("achievements.customer.description"),
+    condition: (userCards) => userCards.length >= 5,
+  },
+  {
+    title: t("achievements.cardsMagnate.title"),
+    description: t("achievements.cardsMagnate.description"),
+    condition: (userCards) => userCards.length >= 9,
+  },
+  {
+    title: t("achievements.collector.title"),
+    description: t("achievements.collector.description"),
+    condition: function (userCards, userPuzzleSets) {
+      return userPuzzleSets.some((set) => set?.collected === PUZZLES_IN_SET);
     },
-    {
-        title: t('achievements.cardsMagnate.title'),
-        description: t('achievements.cardsMagnate.description'),
-        condition: (userCards) => userCards.length >= 9
-    },
-    {
-        title: t('achievements.collector.title'),
-        description: t('achievements.collector.description'),
-        condition: function (userCards, userPuzzleSets) {            
-            return userPuzzleSets.some(set => set?.collected === PUZZLES_IN_SET);
-        }
-    },
-    {
-        title: t('achievements.beginner.title'),
-        description: t('achievements.beginner.description'),
-        condition: (userCards, userPuzzleSets, userData) => userData.xp >= 350
-    },
-    {
-        title: t('achievements.ineedmore.title'),
-        description: t('achievements.ineedmore.description'),
-        condition: (userCards, userPuzzleSets, userData) => userData.xp >= 1000
-    },
-    {
-        title: t('achievements.myprecious.title'),
-        description: t('achievements.myprecious.description'),
-        condition: (userCards, userPuzzleSets, userData) => userData.xp >= 2500
-    },
-    {
-        title: t('achievements.tester.title'),
-        description: t('achievements.tester.description'),
-        condition: (userCards, userPuzzleSets, userData) => userData.level >= 5
-    },
-    {
-        title: t('achievements.opportunist.title'),
-        description: t('achievements.opportunist.description'),
-        condition: (userCards, userPuzzleSets, userData) => userData.level >= 10
-    },
-    {
-        title: t('achievements.champion.title'),
-        description: t('achievements.champion.description'),
-        condition: (userCards, userPuzzleSets, userData) => userData.level === MAX_LEVEL
-    },
-]
+  },
+  {
+    title: t("achievements.beginner.title"),
+    description: t("achievements.beginner.description"),
+    condition: (userCards, userPuzzleSets, userData) => userData.xp >= 350,
+  },
+  {
+    title: t("achievements.ineedmore.title"),
+    description: t("achievements.ineedmore.description"),
+    condition: (userCards, userPuzzleSets, userData) => userData.xp >= 1000,
+  },
+  {
+    title: t("achievements.myprecious.title"),
+    description: t("achievements.myprecious.description"),
+    condition: (userCards, userPuzzleSets, userData) => userData.xp >= 2500,
+  },
+  {
+    title: t("achievements.tester.title"),
+    description: t("achievements.tester.description"),
+    condition: (userCards, userPuzzleSets, userData) => userData.level >= 5,
+  },
+  {
+    title: t("achievements.opportunist.title"),
+    description: t("achievements.opportunist.description"),
+    condition: (userCards, userPuzzleSets, userData) => userData.level >= 10,
+  },
+  {
+    title: t("achievements.champion.title"),
+    description: t("achievements.champion.description"),
+    condition: (userCards, userPuzzleSets, userData) =>
+      userData.level === MAX_LEVEL,
+  },
+];
